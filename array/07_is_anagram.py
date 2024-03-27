6a1a3aba8f1a2e684647d48189ea7fa1b50384e1
Amr Khalil <amr.alshemaly93@gmail.com>
2024-03-27 16:27:01 +0100

    add anagrams

--- array/07_is_anagram.py
+++ array/07_is_anagram.py
@@ -1,15 +1,18 @@
 
-def is_anagram(arr, size):
+def is_anagram(str1, str2):
     """
-    [1,2,3,4], 2 => [[1,2], [3,4]]
-    [1,2,3,4,5], 3 => [[1,2], [3,4], [5]]
+    anagram is a word or phrase formed by rearranging
+    the letters of a different word or phrase,
+    typically using all the original letters exactly once.
+    hello -> olleh = True
+    hello -> world = False
     """
-    ans = []
-    for i in range(0, len(arr), size):
-        ans.append(arr[i:i+size])
     
-    return ans
+    str1 = "".join(sorted(list(str1)))
+    str2 = "".join(sorted(list(str2)))
+    return str1 == str2
 
 
 if __name__ == "__main__":
-    print(is_anagram([1, 2, 3, 4, 5], 3))
+    print(is_anagram("hello", "olleh"))
+    print(is_anagram("hello", "world"))
