ba94d3df816acd20b6c0a7407a4175efba743552
Amr Khalil <amr.alshemaly93@gmail.com>
2024-03-29 14:10:22 +0100

    solve matrix problem

--- array/12_matrix.py
+++ array/12_matrix.py
@@ -16,5 +16,24 @@ def matrix(n):
         ans.append(row)
             
     print(ans)
+
+def matrix2(n):
+    """
+    r*n + c
+    0*3 + 0 = 0
+    0*3 + 1 = 1
+    0*3 + 2 = 2
+    1*3 + 0 = 3
+    1*3 + 1 = 4
+    1*3 + 2 = 5
+    2*3 + 0 = 6
+    2*3 + 1 = 7
+    2*3 + 2 = 8
+    """
+    ans = [[r*n + c for c in range(n)] for r in range(n)]
+    print(ans)
+
 if __name__ == "__main__":
-    matrix(3)
+    matrix2(3)
+    # ans = [[i*n + j for j in range(n)] for i in range(n)]
+    # print(ans)
