8fd1df033795374e831a218d66ec3eacf1c0a6df
Amr Khalil <amr.alshemaly93@gmail.com>
2024-05-17 09:30:23 +0200

    problem solving

--- problem_solving/linked_list/04_merge_two_lists.py
+++ problem_solving/linked_list/04_merge_two_lists.py
@@ -32,12 +32,30 @@ class ListNode(object):
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-        
+
+
+def mergeTwoLists(l1, l2):
+    """
+    time complexity: O(n) because we are visiting each node once
+    space complexity: O(n) because of the recursion
+    """
+    
+    if not l1:
+        return l2
+    if not l2:
+        return l1
+    
+    if l1.val < l2.val:
+        l1.next = mergeTwoLists(l1.next, l2)
+        return l1
+    else:
+        l2.next = mergeTwoLists(l1, l2.next)
+        return l2
+
 def mergeTwoLists1(list1, list2):
     """
-    :type l1: ListNode
-    :type l2: ListNode
-    :rtype: ListNode
+    time complexity: O(n) because we are visiting each node once
+    space complexity: O(1) because we are not using any extra space
     """
     
     head = ListNode()
