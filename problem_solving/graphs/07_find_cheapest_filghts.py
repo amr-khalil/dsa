f5ef6119dff2ea5296acc890703756e6c7b6501f
Amr Khalil <amr.alshemaly93@gmail.com>
2024-06-02 08:32:29 +0200

    problem solving: 787. Cheapest Flights Within K Stops

---
+++ problem_solving/graphs/07_find_cheapest_filghts.py
@@ -0,0 +1,95 @@
+"""
+787. Cheapest Flights Within K Stops
+Medium
+There are n cities connected by some number of flights. You are given an array flights where flights[i] = [fromi, toi, pricei] indicates that there is a flight from city fromi to city toi with cost pricei.
+You are also given three integers src, dst, and k, return the cheapest price from src to dst with at most k stops. If there is no such route, return -1.
+
+Example 1:
+Input: n = 4, flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]], src = 0, dst = 3, k = 1
+Output: 700
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 3 is marked in red and has cost 100 + 600 = 700.
+Note that the path through cities [0,1,2,3] is cheaper but is invalid because it uses 2 stops.
+
+Example 2:
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 1
+Output: 200
+Explanation:
+The graph is shown above.
+The optimal path with at most 1 stop from city 0 to 2 is marked in red and has cost 100 + 100 = 200.
+
+Example 3:
+Input: n = 3, flights = [[0,1,100],[1,2,100],[0,2,500]], src = 0, dst = 2, k = 0
+Output: 500
+Explanation:
+The graph is shown above.
+The optimal path with no stops from city 0 to 2 is marked in red and has cost 500.
+ 
+Constraints:
+1 <= n <= 100
+0 <= flights.length <= (n * (n - 1) / 2)
+flights[i].length == 3
+0 <= fromi, toi < n
+fromi != toi
+1 <= pricei <= 104
+There will not be any multiple flights between two cities.
+0 <= src, dst, k < n
+src != dst
+"""
+
+from collections import defaultdict
+import heapq
+
+def findCheapestPrice(n: int,
+                      flights: list[list[int]],
+                      src: int,
+                      dst: int,
+                      k: int) -> int:
+    """
+    Approach: Dijkstra's algorithm
+    Time complexity: O(VlogV + E), because we visit each node once and each edge once, and we use a priority queue
+    Space complexity: O(V + E), because we store the graph as an adjacency list
+    """
+    graph = defaultdict(dict)
+    for a, b, price in flights:
+        graph[a][b] = price
+    heap = [(0, src, k + 1)] # (price, city, stops)
+    while heap:
+        price, city, stops = heapq.heappop(heap)
+        if city == dst:
+            return price
+        if stops > 0:
+            for neighbor in graph[city]:
+                new_price = price + graph[city][neighbor]
+                heapq.heappush(heap, (new_price, neighbor, stops - 1))
+    return -1
+
+if __name__ == "__main__":
+    n = 4
+    flights = [[0,1,100],[1,2,100],[2,0,100],[1,3,600],[2,3,200]]
+    src = 0
+    dst = 3
+    k = 1
+    print(findCheapestPrice(n, flights, src, dst, k))  # 700
+
+    n = 3
+    flights = [[0,1,100],[1,2,100],[0,2,500]]
+    src = 0
+    dst = 2
+    k = 1
+    print(findCheapestPrice(n, flights, src, dst, k))  # 200
+
+    n = 3
+    flights = [[0,1,100],[1,2,100],[0,2,500]]
+    src = 0
+    dst = 2
+    k = 0
+    print(findCheapestPrice(n, flights, src, dst, k))  # 500
+
+    n = 3
+    flights = [[0,1,100],[1,2,100],[0,2,500]]
+    src = 0
+    dst = 2
+    k = 2
+    print(findCheapestPrice(n, flights, src, dst, k))  # 200
